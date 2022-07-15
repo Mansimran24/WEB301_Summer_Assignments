6898336dd79c741395049be78e700b48e2f2fe00
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-07-15 10:49:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 /*
-  Your name here
+  Mansimran Kaur
+  0786967
+  2022-07-15
 */
+$(function () {
+  function getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, showError);
+    } else {
+      $('#error').text("Geolocation is not supported on your browser!");
+    }
+    function success(position) {
+      $("#latitude").val(position.coords.latitude);
+      $("#longitude").val(position.coords.longitude);
+    }
+    function showError(error) {
+      $('#error').text("Location permission denied");
+    }
+  }
+
+  $("#btnP").on("click", function () {
+    getCurrentLocation();
+  });
+  $("#btnSubmit").on("click", function () {
+    localStorage.setItem("firstname", $("#firstname").val());
+    localStorage.setItem("lastname", $("#lastname").val());
+    localStorage.setItem("longitude", $("#longitude").val());
+    localStorage.setItem("latitude", $("#latitude").val());
+    $('#success').text("Information submitted successfully");
+  });
+
+});
